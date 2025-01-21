44ca16d299c4c070f4ff8f3822c5ebfa0277dcc4
wilky-tech <wilkysatriawan9@gmail.com>
2025-01-21 16:54:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Toggle class active untuk menu
+const navbarnav = document.querySelector(".navbar-nav");
+// Ketika menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingcart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const menu = document.querySelector("#menu");
+const sb = document.querySelector("#search-button");
+const sp = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sp.contains(e.target) && !shoppingcart.contains(e.target)) {
+    shoppingcart.classList.remove("active");
+  }
+});
